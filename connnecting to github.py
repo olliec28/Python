893d1b05672b752d4cdf31ab8131965f47b8c9c1
olliec28 <56151899+olliec28@users.noreply.github.com>
2019-10-11 09:56:09 +0100

    Add files via upload

---
+++ connnecting to github.py
@@ -0,0 +1,40 @@
+print ("Hello world")
+x = 10
+y = 5
+z = 0
+print("x add y = ", x + y)
+x = 7
+y = 8
+z = 0
+print("x add y = " + str(x + y))
+x = 21
+y = 9
+z = 0
+print("(x add y) multiplied by y ", (x + y) * y)
+x = -32
+y = 6
+z = 0
+print("x subtract y = ", x - y)
+x = 34
+y = 3
+z = 0
+print("x multiplied by y = ", x * y)
+print ("x multiplied by y = " + str(x * y))
+x = 21
+y = 7
+z = 0
+print("x divided by y ", x / y)
+x = 45
+y = 6
+z = 0
+print("x modulus y =", x % y)
+x = 45
+y = 6
+z = 0
+print("x floor division y = ", x // y)
+
+
+
+
+
+
